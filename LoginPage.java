422d4a45da85fcfa0d49516037edbd257286e480
SayeGorgor <112979482+SayeGorgor@users.noreply.github.com>
2024-10-17 11:15:34 -0400

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -9,22 +9,9 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class LoginPage extends Application {
-    HashMap<String, String> loginInfo = new HashMap<String, String>();
-    UsernamesAndPasswords logins = new UsernamesAndPasswords();
-    public void setLoginInfo(HashMap<String, String> loginInfo) {
-        this.loginInfo = loginInfo;
-    }
-    /*LoginPage(HashMap<String, String> loginInfoOriginal) throws Exception{
-        loginInfo = loginInfoOriginal;
-        load();
-    }
-     */
-
     @Override
     public void start(Stage stage) throws IOException {
-        UsernamesAndPasswords logins = new UsernamesAndPasswords();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
-        loginInfo = logins.getLoginInfo();
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("SKG Airlines");
