7087b8ecf0b3676267a6c8c48c0d6d58b0e50560
SayeGorgor <112979482+SayeGorgor@users.noreply.github.com>
2024-10-17 11:13:10 -0400

    Update ARSController.java

--- ARSController.java
+++ ARSController.java
@@ -1,21 +1,27 @@
 package com.example.airlinereservationsystem;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
-public class ARSController extends LoginPageController implements Initializable{
+public class ARSController extends LoginPageController implements Initializable {
+    //List of available cities
     ObservableList<String> options = FXCollections.observableArrayList("Atlanta", "Miami", "Chicago", "Houston",
             "New York City", "Los Angeles", "Las Vegas");
     String[] cities = {"Atlanta", "Miami", "Chicago", "Houston", "New York City", "Los Angeles",
@@ -24,6 +30,10 @@ public class ARSController extends LoginPageController implements Initializable{
     HashMap<String, double[]> cityCoords = cityList.getCityCoords();
     double price;
     double distance;
+    private Parent root;
+    private final String[] accountOptionsList = {"Sign Out"};
+    @FXML
+    Stage stage;
     @FXML
     private ComboBox<String> fromBox;
     @FXML
@@ -35,13 +45,20 @@ public class ARSController extends LoginPageController implements Initializable{
     @FXML
     private Label errorMessage;
     @FXML
-    private Label usernameLabel;
+    private Hyperlink usernameLabel;
     @FXML
     private Button calculatePrice;
     @FXML
     private DatePicker departDate;
     @FXML
     private DatePicker returnDate;
+    @FXML
+    private ListView<String> accountOptions;
+
+    public ARSController() throws FileNotFoundException {
+    }
+
+    //Round Trip Box selection
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         fromBox.setItems(FXCollections.observableList(options));
@@ -50,6 +67,7 @@ public class ARSController extends LoginPageController implements Initializable{
         tripBox.getSelectionModel().selectFirst();
         usernameLabel.setText(username);
     }
+    //Calculates the price when the button is hit if all fields are full
     @FXML
     protected void onCalculatePriceButtonPress(ActionEvent event) {
         if(fromBox.getValue() == null || toBox.getValue() == null) {
@@ -70,6 +88,27 @@ public class ARSController extends LoginPageController implements Initializable{
             priceLabel.setText("$" + String.format("%.2f", price));
         }
     }
+    @FXML
+    protected void onUsernameLabelPress(ActionEvent event) throws IOException {
+        accountOptions.setVisible(!accountOptions.isVisible());
+        accountOptions.getItems().setAll(accountOptionsList);
+        accountOptions.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
+                try {
+                    root = fxmlLoader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+        });
+    }
+    //calculates the distance between 2 cities
     public double calculateDistance(double long1, double lat1, double long2, double lat2) {
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(long2 - long1);
@@ -82,5 +121,8 @@ public class ARSController extends LoginPageController implements Initializable{
         double rad = 6371;
         double c = 2 * Math.asin(Math.sqrt(a));
         return rad * c;
+    }
+    public void displayName(String name) {
+        usernameLabel.setText(name);
     }
 }
